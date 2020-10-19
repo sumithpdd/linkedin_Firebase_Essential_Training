b1f83b57d352b582993738d4616ffb2faa6ad85c
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-19 11:38:48 +0200

    Initial commit with working react app

---
+++ restaurant_reservation/README.md
@@ -0,0 +1,33 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### install firebase
+
+to setup
+npm install
+npm i firebase
+
+run npm start
+
+### Install Firebase CLI tools
+
+npm install -g firebase-tools
+or mac
+curl -sL https://firebase.tools | bash
+
+then run
+> firebase login
+
+check the list of projects firebase project
+> firebase projects:list
---
+++ restaurant_reservation/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import './index.css';
+import { App, serviceWorker } from './app';
+
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB1PLYTzXSPTCz9vq2_ykk9270y_p8eFLI",
+    authDomain: "todo-flutter-8a80f.firebaseapp.com",
+    databaseURL: "https://todo-flutter-8a80f.firebaseio.com",
+    projectId: "todo-flutter-8a80f",
+    storageBucket: "todo-flutter-8a80f.appspot.com",
+    messagingSenderId: "965292851106",
+    appId: "1:965292851106:web:4887dd37f3c40df16dfcfb",
+    measurementId: "G-CTVJH3FPBX"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// This is where the magic happens. React renders our App component
+// inside the div with the id "root"
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
